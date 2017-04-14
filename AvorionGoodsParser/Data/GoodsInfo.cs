e9643c55ee8deecf1e6219f418cdd54d49c65984
antis <antis28@mail.ru>
2017-04-14 17:36:13 +0300

    feature(GoodsInfo) содержит информация о ячейке

---
+++ AvorionGoodsParser/Data/GoodsInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvorionGoodsParser.Data
+{
+    class GoodsInfo    
+    {
+        
+
+        public GoodsInfo()
+        {
+        }
+
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Volume { get; set; }
+        public string isIllegal { get; set; }
+        public string isDangerous { get; set; }
+
+        public string[] SoldBy  { get; set; }
+        public string[] BoughtBy  { get; set; }
+        
+        void dsds()
+        {
+            SoldBy.Contains("d");
+        }
+
+    }
+}
