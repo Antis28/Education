b389411ccce096b9a4b953f8ca0e4282d726be09
antis <antis28@mail.ru>
2017-04-14 17:37:12 +0300

    feature(SiteParser) парсит html таблицу с товарами

---
+++ AvorionGoodsParser/Parser/HtmlString.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvorionGoodsParser.Parser
+{
+    class HtmlString
+    {
+        // Объявляем событие для индикатора
+        public event Action ChangeValueEvent;
+        public event Action<int> MaxValueEvent;
+        //public event Action<List<JobInfo>> CompleteConvertEvent;
+        public event Action CanceledConvertEvent;
+
+        // Используем метод для запуска события
+        protected void OnChangeValue()
+        {
+            if( ChangeValueEvent != null )
+                ChangeValueEvent();
+        }
+        protected void OnMaxValue( int maxValue )
+        {
+            if( MaxValueEvent != null )
+                MaxValueEvent(maxValue);
+        }
+        //protected void OnCompleteConvert( List<JobInfo> lJobs )
+        //{
+        //    CompleteConvertEvent(lJobs);
+        //}
+        protected void OnCanceledConvert()
+        {
+            if( CanceledConvertEvent != null )
+                CanceledConvertEvent();
+        }
+
+        protected void ClearEvents()
+        {
+            ChangeValueEvent = null;
+            MaxValueEvent = null;
+            //CompleteConvertEvent = null;
+            CanceledConvertEvent = null;
+        }
+
+        protected static string ReadHTML( string site, Encoding encoding )
+        {
+            string result = "";
+            // Создать объект запроса
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(site);
+
+            // Получить ответ с сервера
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // Получаем поток данных из ответа
+            using( StreamReader stream = new StreamReader(
+                 response.GetResponseStream(), encoding) )
+            {
+                // Выводим исходный код страницы
+                result = stream.ReadToEnd();
+            }
+            using( StreamWriter streawr = new StreamWriter(
+                Environment.CurrentDirectory + "\\" + "Goods.txt"
+                , false
+                , encoding) )
+            {
+                streawr.WriteLine(result);
+            }
+
+
+
+            //
+            // Получаем некоторые данные о сервере
+            //string messageServer = "Целевой URL: \t" + 
+            //                            request.RequestUri + 
+            //                            "\nМетод запроса: \t" + 
+            //                            request.Method +
+            //                            "\nТип полученных данных: \t" + 
+            //                            response.ContentType + 
+            //                            "\nДлина ответа: \t" + 
+            //                            response.ContentLength + 
+            //                            "\nЗаголовки";
+            //Console.WriteLine( messageServer );
+            //
+
+
+            return result;
+        }
+        protected static string ReadHTML( string site = "http://www.professorweb.ru" )
+        {
+            return ReadHTML(site, Encoding.UTF8);
+        }
+        protected string ReadFileHTML( string file, Encoding encoding = null )
+        {
+            string result = "";
+
+            if( encoding == null )
+                encoding = Encoding.UTF8;
+
+            // Получаем поток данных из ответа
+            using( StreamReader stream = new StreamReader(
+                Environment.CurrentDirectory + "\\" + file
+                 , encoding) )
+            {
+                // Выводим исходный код страницы
+                result = stream.ReadToEnd();
+            }
+            return result;
+        }
+    }
+}
--- AvorionGoodsParser/Parser/SiteParser.cs
+++ AvorionGoodsParser/Parser/SiteParser.cs
@@ -2,11 +2,123 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
+using AvorionGoodsParser.Data;
 
 namespace AvorionGoodsParser.Parser
 {
-    class SiteParser
+    class SiteParser : HtmlString
     {
+        public void BeginParse()
+        {
+            Thread backgroundThread = new Thread(new ThreadStart(Start));
+            backgroundThread.Name = "SiteParser";
+            backgroundThread.IsBackground = true;
+            backgroundThread.Start();
+        }
+
+        private void Start()
+        {
+            //OnCompleteConvert(GetJobList());
+            GetGoodsList();
+        }
+
+        public List<GoodsInfo> GetGoodsList()
+        {
+            List<GoodsInfo> GoodsList = new List<GoodsInfo>();
+
+            //string siteAddress = "http://avorion.gamepedia.com/index.php?title=Goods";
+            string siteAddress = "Goods.txt";//"Goods - Official Avorion Wiki.htm";            
+            //xPathQuery
+            string xpq_allWorks = "//table[@class=\"wikitable sortable\"]/tr";          //<table class="wikitable sortable">   //<div id="dle-content">//class="rabota-all"
+           
+            HtmlDocument allHTML = new HtmlDocument();
+            HtmlDocument currentHTML = new HtmlDocument();
+
+            //allHTML.LoadHtml(ReadHTML(siteAddress));
+            allHTML.LoadHtml(ReadFileHTML(siteAddress));
+            var TableNodes = allHTML.DocumentNode.SelectNodes(xpq_allWorks);
+
+            //OnMaxValue(TableNodes.Count());            
+            foreach( var lineNode in TableNodes )
+            {
+                if( lineNode == TableNodes[0] )
+                    continue;
+                HtmlNodeCollection tdNodes = lineNode.SelectNodes("td");
+
+                string[] soldByArr = NodeToStringArr(tdNodes[3].SelectNodes("a"), out soldByArr);
+                string[] boughtByArr = NodeToStringArr(tdNodes[4].SelectNodes("a"), out boughtByArr);
+                if( tdNodes.Count < 7)
+                {                    
+                    continue;
+                }
+                
+                GoodsList.Add(new GoodsInfo
+                {
+                    Name = tdNodes[0].InnerText.Replace("\r\n", ""),
+                    Volume = tdNodes[1].InnerText.Replace("\r\n", ""),
+                    Price = tdNodes[2].InnerText.Replace("\r\n", ""),
+                    SoldBy = soldByArr,
+                    BoughtBy = boughtByArr,
+                    isIllegal = tdNodes[5].InnerText.Replace("\r\n", ""),
+                    isDangerous = tdNodes[6].InnerText.Replace("\r\n", "")
+                });                           
+                OnChangeValue();
+            }
+
+            return GoodsList;
+        }
+        
+        private static string[] NodeToStringArr(HtmlNodeCollection selectedCollection, out string[] soldByArr )
+        {
+            if( selectedCollection != null )
+            {
+                soldByArr = new string[selectedCollection.Count];
+                for( int i = 0; i < soldByArr.Length; i++ )
+                {
+                    soldByArr[i] = selectedCollection[i].InnerText;
+                }
+            }
+            else
+                soldByArr = new string[0];
+
+            return soldByArr;
+        }
+
+        private string searchByTitle( string URL, string title, string xPath )
+        {
+            HtmlDocument curHtmlDocument = new HtmlDocument();
+            string curHtml = ReadHTML(URL);
+            curHtmlDocument.LoadHtml(curHtml);
+            xPath = "//div[@class=\"rabota-allfull\"]/div[@class=\"rabota-fields\"]";
+            HtmlNodeCollection divNodes = curHtmlDocument.DocumentNode.SelectNodes(xPath);
+            foreach( var item in divNodes )
+            {
+                bool isContain = item.InnerText.Contains(title);
+                string findedElement = item.InnerText;
+                if( isContain )
+                {
+                    //findedElement = findedElement.Replace("&nbsp;", " ");
+                    findedElement = findedElement.Replace(title, "");
+                    return findedElement;
+                }
+            }
+            return "";
+        }
+
+        private string ExtractSalary( HtmlDocument currentHTML )
+        {
+            HtmlNode priseNode = currentHTML.DocumentNode.ChildNodes[1].ChildNodes[5];
+            string salary = priseNode.InnerHtml;
+            string price = "Не указано";
+            if( salary.Contains("Зарплата") )
+            {
+                price = priseNode.SelectSingleNode("font[@class=\"rabota-fieldsb\"]").InnerText;
+            }
+
+            return price;
+        }
     }
 }
