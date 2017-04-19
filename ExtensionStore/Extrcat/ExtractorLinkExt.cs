e33bc180b31071d98c05085c25b1e87f88d82777
antis <antis28@mail.ru>
2017-04-19 21:05:16 +0300

    add(ExtractorLinkExt) ->
    *  события для трех стадий парсинга
    *  парсинг страницы категории файлов
    *  парсинг страницы файла

--- ExtensionStore/Extrcat/ExtractorLinkExt.cs
+++ ExtensionStore/Extrcat/ExtractorLinkExt.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 using HtmlAgilityPack;
 
 namespace ExtensionStore
@@ -19,12 +21,75 @@ namespace ExtensionStore
 
         public Encoding encoding;
 
-        public new event Action<Dictionary<string, string>> CompleteConvertEvent;
-        protected void OnCompleteConvert( Dictionary<string, string> list )
+        ///////////////////////////////////////////////////////////////////////
+
+        //события для ссылок на категории форматов
+        public event Action<Dictionary<string, string>> CompleteGenLinkParseEvent;
+        public event Action<int> MaxValueGeneralEvent;
+        public event Action ChangeValueGeneralEvent;
+
+        protected void OnMaxValueGeneral( int maxValue )
         {
-            CompleteConvertEvent(list);
+            if( MaxValueGeneralEvent != null )
+                MaxValueGeneralEvent(maxValue);
+        }
+        protected void OnChangeValueGen()
+        {
+            if( ChangeValueGeneralEvent != null )
+                ChangeValueGeneralEvent();
+        }
+        protected void OnCompleteGenLinkParse( Dictionary<string, string> list )
+        {
+            CompleteGenLinkParseEvent(list);
         }
 
+        ///////////////////////////////////////////////////////////////////////
+
+        //события для ссылок на все форматы
+        public event Action<Dictionary<string, List<string>>> CompleteAllLinkParseEvent;
+        public event Action<int> MaxValueAllEvent;
+        public event Action ChangeValueAllEvent;
+
+        protected void OnCompleteAllLinkParse( Dictionary<string, List<string>> list )
+        {
+            if( CompleteAllLinkParseEvent != null )
+                CompleteAllLinkParseEvent(list);
+        }
+        protected void OnMaxValueAll( int maxValue )
+        {
+            if( MaxValueAllEvent != null )
+                MaxValueAllEvent(maxValue);
+        }
+        protected void OnChangeValueAll()
+        {
+            if( ChangeValueAllEvent != null )
+                ChangeValueAllEvent();
+        }
+
+        ///////////////////////////////////////////////////////////////////////        
+
+        //события для ссылок на все форматы
+        public event Action<Dictionary<string, List<string>>> CompleteExtParseEvent;
+        public event Action<int> MaxValueExtParseEvent;
+        public event Action ChangeValueExtParseEvent;
+
+        protected void OnCompleteExtParse( Dictionary<string, List<string>> list )
+        {
+            if( CompleteExtParseEvent != null )
+                CompleteExtParseEvent(list);
+        }
+        protected void OnMaxValueExtParse( int maxValue )
+        {
+            if( MaxValueExtParseEvent != null )
+                MaxValueExtParseEvent(maxValue);
+        }
+        protected void OnChangeValueExtParse()
+        {
+            if( ChangeValueExtParseEvent != null )
+                ChangeValueExtParseEvent();
+        }
+
+        ///////////////////////////////////////////////////////////////////////
         public ExtractorLinkExt()
         {
             siteAddress = "http://open-file.ru/";
@@ -48,11 +113,13 @@ namespace ExtensionStore
 
         private void Start()
         {
-            OnCompleteConvert(GetList());
-            //GetGoodsList();
+            Dictionary<string, string> linkList = GetTypeLinkList();
+            OnCompleteGenLinkParse(linkList);
+            Dictionary<string, List<string>> allLinkDict = GetAllLinkList(linkList);
+            GetExtensionList(allLinkDict);
         }
 
-        public Dictionary<string,string> GetList()
+        private Dictionary<string, string> GetTypeLinkList()
         {
             Dictionary<string, string> ExtList = new Dictionary<string, string>();
 
@@ -62,20 +129,20 @@ namespace ExtensionStore
             HtmlDocument allHTML = new HtmlDocument();
             HtmlDocument currentHTML = new HtmlDocument();
 
+            fileTempAddress = "links.txt";
             if( File.Exists(fileTempAddress) )
             {
                 allHTML.LoadHtml(ReadFileHTML(fileTempAddress, encoding));
             }
             else
             {
-                allHTML.LoadHtml(ReadHTML(siteAddress, encoding));
+                allHTML.LoadHtml(ReadHTML(siteAddress, encoding, fileTempAddress));
             }
 
+            var TableNodes = allHTML.DocumentNode.SelectSingleNode(xpq_allWorks).SelectNodes("li");
 
-            var TableNodes = allHTML.DocumentNode.SelectSingleNode(xpq_allWorks);
-
-            //OnMaxValue(TableNodes.Count());            
-            foreach( var lineNode in TableNodes.ChildNodes )
+            OnMaxValueGeneral(TableNodes.Count());
+            foreach( var lineNode in TableNodes )
             {
                 if( lineNode.Name == "#text" )
                     continue;
@@ -85,11 +152,228 @@ namespace ExtensionStore
                 HtmlAttribute href = tdNodes.Attributes["href"];
                 string name = tdNodes.InnerText;
                 string link = href.Value;
-                ExtList.Add(name,link);
-                OnChangeValue();
+                ExtList.Add(name, link);
+                OnChangeValueGen();
+            }
+            return ExtList;
+        }
+        private Dictionary<string, List<string>> GetAllLinkList( Dictionary<string, string> linkList )
+        {
+            Dictionary<string, List<string>> allLinkDict =
+                                            new Dictionary<string, List<string>>();
+
+            HtmlDocument allHTML = new HtmlDocument();
+            HtmlDocument currentHTML = new HtmlDocument();
+
+            //fileTempAddress = "allLink.txt";
+            fileTempAddress = "audio.htm";
+            OnMaxValueAll(linkList.Count);
+            foreach( KeyValuePair<string, string> item in linkList )
+            {
+                fileTempAddress = item.Key + ".txt";
+                List<string> templinkType = new List<string>();
+                if( !allLinkDict.ContainsKey(item.Key) )
+                {
+                    allLinkDict.Add(item.Key, templinkType);
+                }
+                else
+                {
+                    allLinkDict[item.Key] = templinkType;
+                }
+
+                if( File.Exists(fileTempAddress) )
+                {
+                    allHTML.LoadHtml(ReadFileHTML(fileTempAddress, encoding));
+                }
+                else
+                {
+                    allHTML.LoadHtml(ReadHTML(siteAddress + item.Value, encoding, fileTempAddress));
+                }
+                string xpq_allWorks;
+                HtmlNodeCollection TableNodes;
+                xpq_allWorks = "//table[@class=\"tbl tablecol-2\"]/tbody/tr";
+                TableNodes = allHTML.DocumentNode.SelectNodes(xpq_allWorks);
+                if( TableNodes == null )
+                {
+                    xpq_allWorks = "//table[@class=\"tbl tablecol-2\"]/tr";
+                    TableNodes = allHTML.DocumentNode.SelectNodes(xpq_allWorks);
+                    if( TableNodes == null )
+                    {
+                        MessageBox.Show("Ошибка - " + item.Key);
+                        continue;
+                    }
+                }
+                foreach( var lineNode in TableNodes )
+                {
+                    HtmlNodeCollection tdNodes = lineNode.SelectNodes("td");
+                    if( tdNodes == null )
+                        continue;
+                    HtmlNode elA = tdNodes[0].SelectSingleNode("a");
+                    HtmlAttribute href = elA.Attributes["href"];
+                    templinkType.Add(href.Value);
+                }
+                OnChangeValueAll();
             }
 
-            return ExtList;
+            return allLinkDict;
+        }
+        private List<ExtInfo> GetExtensionList( Dictionary<string, List<string>> AllLink )
+        {
+            List<ExtInfo> extList = new List<ExtInfo>();
+
+            OnMaxValueAll(AllLink.Count);
+            foreach( KeyValuePair<string, List<string>> item in AllLink )
+            {
+                OnMaxValueExtParse(item.Value.Count());
+                foreach( var link in item.Value )
+                {
+                    ExtInfo ext = GetDescriptionExtension(link);
+                    extList.Add(ext);
+                    OnChangeValueExtParse();
+                }
+                OnChangeValueAll();
+            }
+            return extList;
+        }
+
+        private ExtInfo GetDescriptionExtension( string link )
+        {
+            ExtInfo ext = new ExtInfo();
+            HtmlDocument allHTML = new HtmlDocument();
+            HtmlDocument currentHTML = new HtmlDocument();
+            string siteAddress = "";
+            if( link.Contains(this.siteAddress) )
+                siteAddress = link;
+            else
+                siteAddress = this.siteAddress + link;
+            ext.Link = siteAddress;
+            fileTempAddress = siteAddress.Replace("http://open-file.ru/types/", "");
+            fileTempAddress = "types\\" + fileTempAddress + ".txt";
+            if( File.Exists(fileTempAddress) )
+            {
+                allHTML.LoadHtml(ReadFileHTML(fileTempAddress, encoding));
+            }
+            else
+            {
+                allHTML.LoadHtml(ReadHTML(siteAddress, encoding, fileTempAddress));
+            }
+            //xPathQuery
+            //table class="desc"
+            string xpq_allWorks;
+            HtmlNodeCollection TableNodes;
+
+            xpq_allWorks = "//table[@class=\"desc\"]/*/*/*/td|//table[@class=\"desc\"]/*/*/td|//table[@class=\"desc\"]/*/td|//table[@class=\"desc\"]/td|//table[@class=\"desc\"]/th";
+            TableNodes = allHTML.DocumentNode.SelectNodes(xpq_allWorks);
+            if( TableNodes == null )
+            {
+                MessageBox.Show("Ошибка - " + link);
+                return null;
+            }
+
+
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+
+            foreach( var tdNode in TableNodes )
+            {
+                string key = null;
+                key = tdNode.InnerText;
+                int index = TableNodes.IndexOf(tdNode);
+                //if( tdNodes == null )
+                //{
+                //    tdNodes = lineNode.SelectNodes("th");
+                //    if( tdNodes.Count <= 2 )
+                //    {
+                //        key = tdNodes[0].InnerText.Replace("\n", "").Replace("\r", "");
+                //        MatchCollection m = Regex.Matches(key, @"([А-я]\w+\s*\w*\s*)(\.*[a-z]*)");//[А-я]\w+\s+\w+
+                //        ext.Header = m[0].Value;
+                //        if( m.Count > 1 )
+                //            key = m[1].Value;
+                //        m = Regex.Matches(tdNodes[0].InnerText, @"\.[a-z]*");//[А-я]\w+\s+\w+
+                //        if( m.Count > 0 )
+                //            val = m[0].Value;
+                //    }
+                //    //continue;
+                //}
+                //else
+                //{
+                //    if( tdNodes.Count == 2 )
+                //    {
+                //        key = tdNodes[0].InnerText;
+                //        val = tdNodes[1].InnerText;
+                //    }
+                //    else if( tdNodes.Count == 1 )
+                //    {
+                //        key = tdNodes[0].InnerText;
+                //        val = null;
+                //    }
+                //}
+                switch( key )
+                {
+                    case "Информация о заголовке файла ":
+                        //ext.InfoHeaderFile.Add(TableNodes[index + 1].InnerText);
+                        break;
+                    case "Тип файла":
+                        ext.TypeFile = TableNodes[index + 1].InnerText;
+                        break;
+                }
+                if( key.Contains("Формат") )
+                {
+                    Match m = Regex.Match(tdNode.InnerText, @"\.[a-zа-я0-9]*");
+                    ext.Name = m.Value;
+                    ext.Header = m.Value;
+                }
+
+                if( key.Contains("ASCII:") )
+                {
+                    ext.InfoHeaderFile.Add(key);
+                }
+                if( key.Contains("HEX:") )
+                {
+                    ext.InfoHeaderFile.Add(key);
+                }
+                
+                if( key.Contains("на русском") )
+                {
+                    ext.RusDescription = TableNodes[index + 1].InnerText;
+                }
+                if( key.Contains("на английском") )
+                {
+                    ext.EngDescription = TableNodes[index + 1].InnerText;
+                }
+                if( key == " Windows" || key == "Windows" )
+                {
+                    ext.WhatOpen = TableNodes[index + 1].InnerText;
+
+                    HtmlNodeCollection li_s = TableNodes[index + 1].SelectNodes("*/li|*/*/li");
+                    foreach( HtmlNode li in li_s )
+                    {
+                        ext.WhatOpenWindows.Add(li.InnerText);
+                    }
+                }
+                if( key == " Linux" || key == "Linux" )
+                {
+                    ext.WhatOpen += TableNodes[index + 1].InnerText;
+                    HtmlNodeCollection li_s = TableNodes[index + 1].SelectNodes("*/li");
+
+                    foreach( HtmlNode li in li_s )
+                    {
+                        ext.WhatOpenLinux.Add(li.InnerText);
+                    }
+
+                }
+                if( key.Contains("MacOS") )
+                {
+                    ext.WhatOpen += TableNodes[index + 1].InnerText;
+                    HtmlNodeCollection li_s = TableNodes[index + 1].SelectNodes("*/li");
+                    foreach( HtmlNode li in li_s )
+                    {
+                        ext.WhatOpenLinux.Add(li.InnerText);
+                    }
+                }
+                //dic.Add(key, val);                
+            }
+            return ext;
         }
     }
 }
+//OnChangeValue();
