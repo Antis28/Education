b9dcc09d5863ad58c029f88cafd4f809b2264543
antis <antis28@mail.ru>
2017-04-19 21:02:23 +0300

    add(ParserManeger) обработчики событий

--- ExtensionStore/Extrcat/ParserManeger.cs
+++ ExtensionStore/Extrcat/ParserManeger.cs
@@ -20,22 +20,31 @@ namespace ExtensionStore
         }
         public void Parse()
         {
-            //Ссылки на категории расширений
-            List<string> linkCategoryList = new List<string>();
-            //Имя катекории, список расширений
-            Dictionary<string, List<string>> linkAllExtensonDictionary;
-
             Encoding codePage = Encoding.GetEncoding(1251);
 
-            ExtractorLinkExt extractor = new ExtractorLinkExt("http://open-file.ru/", codePage);
+            ExtractorLinkExt extractor = new ExtractorLinkExt("http://open-file.ru", codePage);
+
+            //события для ссылок на категории форматов
             extractor.CompleteGenLinkParseEvent += Extractor_CompleteConvertEvent;
+            extractor.ChangeValueGeneralEvent += Extractor_ChangeValueGeneralEvent;
+            extractor.MaxValueGeneralEvent += Extractor_MaxValueGeneralEvent;
+
+            //события для ссылок всех форматов
+            extractor.MaxValueAllEvent += Extractor_MaxValueAllEvent;
+            extractor.ChangeValueAllEvent += Extractor_ChangeValueAllEvent;
+            extractor.CompleteAllLinkParseEvent += Extractor_CompleteAllLinkParseEvent;
+
+            //события для ссылок всех форматов
+            extractor.MaxValueExtParseEvent += Extractor_MaxValueExtParseEvent; ;
+            extractor.ChangeValueExtParseEvent += Extractor_ChangeValueExtParseEvent; ;
+            extractor.CompleteExtParseEvent += Extractor_CompleteExtParseEvent; ;
+
             extractor.BeginParse();
 
             ExtParser parser = new ExtParser("", codePage);
-            parser.fileTempAddress = "psd.htm";
-            parser.CompleteExtractLinkEvent += Parser_CompleteConvertEvent;
-            
-            parser.BeginParse();
+            //parser.fileTempAddress = "psd.htm";
+            //parser.CompleteExtractLinkEvent += Parser_CompleteConvertEvent;
+            //parser.BeginParse();
 
             #region Create XML            
             ///////////////////////////////////////////////////////////////////////////////////// 
@@ -97,26 +106,73 @@ namespace ExtensionStore
 
         }
 
-        private void Parser_CompleteConvertEvent( ExtInfo obj )
+        private void Extractor_CompleteExtParseEvent( Dictionary<string, List<string>> obj )
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Extractor_ChangeValueExtParseEvent()
         {
             mainWindow.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal,
-                    (Action)delegate
-                    {
-                        mainWindow.listBox.Items.Add(obj.Name);
-                        mainWindow.listBox.Items.Add(obj.Header);
-                        mainWindow.listBox.Items.Add(obj.TypeFile);
-                        
-                        mainWindow.listBox.Items.Add(obj.RusDescription);
-                        mainWindow.listBox.Items.Add(obj.EngDescription);
-
-                        mainWindow.listBox.Items.Add(obj.InfoHeaderFile);
-                        foreach( var item in obj.WhatOpenWindows )
-                        {
-                            mainWindow.listBox.Items.Add(item);
-                        }
-                        
-                    });
+                  (Action)delegate
+                  {
+                      mainWindow.pb_allExtension.Value += 1;
+                  });
         }
+
+        private void Extractor_MaxValueExtParseEvent( int obj )
+        {            
+            mainWindow.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal,
+                   (Action)delegate
+                   {
+                       mainWindow.tb_allExtension.Text = "Форматов: " + obj;
+                       mainWindow.pb_allExtension.Maximum = obj;
+                       mainWindow.pb_allExtension.Value = 0;
+                   });
+        }
+
+        private void Extractor_CompleteAllLinkParseEvent( Dictionary<string, List<string>> obj )
+        {
+            System.Windows.MessageBox.Show("Fine");            
+        }
+
+        private void Extractor_ChangeValueAllEvent()
+        {
+            mainWindow.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal,
+                   (Action)delegate
+                   {
+                       mainWindow.pb_allLinks.Value += 1;
+                   });
+        }
+
+        private void Extractor_MaxValueAllEvent( int obj )
+        {
+            mainWindow.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal,
+                   (Action)delegate
+                   {
+                       mainWindow.tb_allLinks.Text = "Ссылки на формат: " + obj;
+                       mainWindow.pb_allLinks.Maximum = obj;
+                       mainWindow.pb_allLinks.Value = 0;
+                   });
+        }
+
+        private void Extractor_ChangeValueGeneralEvent()
+        {
+            mainWindow.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal,
+                   (Action)delegate
+                   {
+                       mainWindow.pb_genLinks.Value += 1;
+                   });
+        }
+        private void Extractor_MaxValueGeneralEvent( int obj )
+        {
+            mainWindow.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal,
+                   (Action)delegate
+                   {
+                       mainWindow.tb_genLinks.Text = "Общие ссылки: " + obj;
+                       mainWindow.pb_genLinks.Maximum = obj;
+                   });
+        }       
 
         private void Extractor_CompleteConvertEvent( Dictionary<string, string> obj )
         {
@@ -126,7 +182,28 @@ namespace ExtensionStore
                         foreach( KeyValuePair<string, string> item in obj )
                         {
                             mainWindow.listBox.Items.Add(item.Key);
-                        }                        
+                        }
+                    });
+        }
+
+        private void Parser_CompleteConvertEvent( ExtInfo obj )
+        {
+            mainWindow.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal,
+                    (Action)delegate
+                    {
+                        //mainWindow.listBox.Items.Add(obj.Name);
+                        //mainWindow.listBox.Items.Add(obj.Header);
+                        //mainWindow.listBox.Items.Add(obj.TypeFile);
+
+                        //mainWindow.listBox.Items.Add(obj.RusDescription);
+                        //mainWindow.listBox.Items.Add(obj.EngDescription);
+
+                        //mainWindow.listBox.Items.Add(obj.InfoHeaderFile);
+                        //foreach( var item in obj.WhatOpenWindows )
+                        //{
+                        //    mainWindow.listBox.Items.Add(item);
+                        //}
+
                     });
         }
 
@@ -148,7 +225,7 @@ namespace ExtensionStore
                     try
                     {
                         ignorDuplicates.Add(item, pairKV.Key);
-                    } catch( Exception  ) { }
+                    } catch( Exception ) { }
                 }
             }
             ;
@@ -180,7 +257,7 @@ namespace ExtensionStore
                     continue;
                 nedostaushie.Add(pairKV.Key, "");
             }
-            
+
             ignorDuplicates = nedostaushie;
             ////////////////////////////////////////////////////////////////////////////////////////////////////////////
             /////////////////////////////////////////////////////////////////////////////////////////////
