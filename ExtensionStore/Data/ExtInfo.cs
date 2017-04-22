11e0c9f28c862d6d94ed8ee1e74183e8c8e31c7e
antis <antis28@mail.ru>
2017-04-22 21:32:22 +0300

    add(ExtInfo) сложение объектов

--- ExtensionStore/Data/ExtInfo.cs
+++ ExtensionStore/Data/ExtInfo.cs
@@ -19,11 +19,32 @@ namespace ExtensionStore
             RusDescription = String.Empty;
             EngDescription = String.Empty;
             DetailedDescription = String.Empty;
+            TypeFile = String.Empty;
+        }
+        public static ExtInfo operator +( ExtInfo left, ExtInfo right )
+        {
+            ExtInfo temp = new ExtInfo();
+            temp.Name = left.Name;
+            if( left.TypeFile != string.Empty  )
+                temp.TypeFile = left.TypeFile + ", " + right.TypeFile;
+            temp.InfoHeaderFile.AddRange(left.InfoHeaderFile);
+            temp.InfoHeaderFile.AddRange(right.InfoHeaderFile);
+            temp.EngDescription = left.EngDescription + "\n" + right.EngDescription;
+            temp.RusDescription = left.RusDescription + "\n" + right.RusDescription;
+            temp.DetailedDescription = left.DetailedDescription + "\n" + right.DetailedDescription;
+            temp.WhatOpenWindows.AddRange(left.WhatOpenWindows);
+            temp.WhatOpenWindows.AddRange(right.WhatOpenWindows);
+            temp.WhatOpenLinux.AddRange(left.WhatOpenLinux);
+            temp.WhatOpenLinux.AddRange(right.WhatOpenLinux);
+            temp.WhatOpenMac.AddRange(left.WhatOpenMac);
+            temp.WhatOpenMac.AddRange(right.WhatOpenMac);
+
+            return temp;
         }
 
         public string Name { get; set; }
         public string Link { get; set; }
-        
+
         public string TypeFile { get; set; }
         public string RusDescription { get; set; }
         public string EngDescription { get; set; }
